960bc5bd5737acb0fbbfb38714691ae3410173ca
samiralibabic <samir.alibabic@gmail.com>
2016-05-14 13:16:30 +0200

    Completed HTML & CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
 $(document).ready(function() {
-      
+    
 });
